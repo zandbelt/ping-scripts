579190a71174403d22c33151845db748319bae93
Hans Zandbelt <hzandbelt@pingidentity.com>
2015-01-20 15:11:06 +0100

    added "update" action, refactoring and cleanup

--- pingfed-admin-api/connections-from-xml.sh
+++ pingfed-admin-api/connections-from-xml.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ###########################################################################
-# Copyright (C) 2014 Ping Identity Corporation
+# Copyright (C) 2014-2015 Ping Identity Corporation
 # All rights reserved.
 #
 # For further information please contact:
@@ -33,24 +33,28 @@
 # A Bash shell script to import (or delete from) PingFederate a set of
 # connections specified in an XML metadata file.
 #
+# TODO: expired certificates are refused -> PF/API feature request?
+# TODO: cert order/labeling on (templated) create and update
+# TODO: double-check error handling (return/exit depending on calling in subshell)
+# 
 ##########################################################################
 
 ADMIN_CREDS="administrator:2Federate"
 PF_HOSTPORT="localhost:9999"
 
-CURL_OPTS="-s -k"
+CURL_OPTS="-s -S -k"
 
-TMP1="/tmp/entities.xml"
-TMP2="/tmp/entity.xml"
+TMP_ENTITIES="/tmp/entities.xml"
+TMP_ENTITY="/tmp/entity.xml"
 
-print_usage() {
+function print_usage() {
 	MSG=$1
 	echo "Usage: $0 [-u <admin-user>:<admin-pwd>] [-s <admin-host>:<admin-port>] [-c <metadata-signing-cert>] [-t <template-connection-entity-id>] <metadata-url> create|delete|update "
 	if [[ -n ${MSG} ]] ; then echo " # ERROR: ${MSG}"; fi
 	exit 1
 }
 
-utility_check() {
+function utility_check() {
 	BIN=`which $1`
 	if [[ -z ${BIN} ]] ; then
 		echo " # ERROR: required utility $1 is missing: install it first."
@@ -73,7 +77,7 @@ while getopts ":c:t:u:s:" opt; do
 			CERT=${OPTARG}
 			;;
 		t)
-			TEMPLATE=${OPTARG}
+			TEMPLATE_ENTITY_ID=${OPTARG}
 			;;
 		u)
 			ADMIN_CREDS=${OPTARG}
@@ -101,12 +105,12 @@ if [[ ${ACTION} != "create"  && ${ACTION} != "update" && ${ACTION} != "delete" ]
 	print_usage "unsupported action \"${ACTION}\""
 fi
 
-if [[ -z ${TEMPLATE} && ${ACTION} == "create" ]] ; then
+if [[ -z ${TEMPLATE_ENTITY_ID} && ${ACTION} == "create" ]] ; then
 	print_usage "action \"${ACTION}\" requires the option \"-t <template-connection-entity-id>\""
 fi
 
 echo " # INFO: retrieving metadata file"
-${CURL_BIN} -# ${METADATA_URL} -o ${TMP1}
+${CURL_BIN} -# ${METADATA_URL} -o ${TMP_ENTITIES}
 if [[ $? -ne 0 ]] ; then
 	echo " # ERROR: could not retrieve metadata file from URL: $1!"
 	exit 1
@@ -115,8 +119,8 @@ fi
 # see if we need to verify the signature on the XML file because a certificate was provided
 if [[ -n ${CERT} ]] ; then
 	XMLSEC=`utility_check xmlsec1`
-	if "${XMLSEC}" verify --id-attr:ID "urn:oasis:names:tc:SAML:2.0:metadata:EntitiesDescriptor" --pubkey-cert-pem ${CERT} ${TMP1} 2>/dev/null ; then
-		echo "Succesfully verified the signature on the metadatafile."
+	if "${XMLSEC}" verify --id-attr:ID "urn:oasis:names:tc:SAML:2.0:metadata:EntitiesDescriptor" --pubkey-cert-pem ${CERT} ${TMP_ENTITIES} 2>/dev/null ; then
+		echo "Successfully verified the signature on the metadata file."
 	else
 		echo " # ERROR: signature verification on the metadata file failed!"
 		exit 1
@@ -130,36 +134,22 @@ else
 fi
 
 # execute an xmllint command, optionally filtered
-exec_xmllint() {
+function exec_xmllint() {
 	local XMLFILE=$1
-	local CMD=$2
+	local CMD="setns md=urn:oasis:names:tc:SAML:2.0:metadata\n${2}"
 	local FILTER=$3
-	EXEC="echo \"setns md=urn:oasis:names:tc:SAML:2.0:metadata\n${CMD}\" | ${XMLLINT_BIN} --shell ${XMLFILE}"
-	if [ ! -z "$1" ] ; then
-		EXEC="${EXEC} | ${GREP_BIN} \"${FILTER}\""
-	fi
-	eval ${EXEC}
-}
 
-# create a JSON request object for the connectionMetadata/convert endpoint
-create_convert_json() {
-	local TYPE=$1
-	local PROTO=$2
-	local B64XML=$3
-	cat <<HERE
-{
-  "connectionType" : "${TYPE}",
-  "expectedProtocol" : "${PROTO}",
-  "samlMetadata" : "${B64XML}",
-  "templateConnection" : ${TEMPLATE_CONN}
-}
-HERE
+	if [ -z "${FILTER}" ] ; then
+		echo "${CMD}" | "${XMLLINT_BIN}" --shell "${XMLFILE}"
+	else
+		echo "${CMD}" | "${XMLLINT_BIN}" --shell "${XMLFILE}" | "${GREP_BIN}" "${FILTER}"
+	fi
 }
 
 # perform a request to the Admin API
-api_request() {
-	PATH=$1
-	METHOD="GET"
+function api_request() {
+	local PATH=$1
+	local METHOD="GET"
 	if [ ! -z "$2" ] ; then
 		METHOD=$2
 	fi
@@ -182,92 +172,115 @@ api_request() {
 	fi
 	
 	if echo ${RESPONSE} | ${JQ_BIN} -e '.resultId' >/dev/null ; then
-		echo " # ERROR: API request to ${URL} failed:" >&2
-		echo ${RESPONSE} >&2
+		printf "\n # ERROR: API request to ${URL} failed:\n" >&2
+		echo "${RESPONSE}" | ${JQ_BIN} '.' >&2
 		exit 1
 	fi
 
 	echo "${RESPONSE}"
 }
 
-# create a connection from an EntityDescriptor in an XML file using the Admin REST API
-conn_idp_create() {
-	local FILE=$1
-	
-	# base64encode the entity descriptor XML
-	B64XML=`cat ${FILE} | ${OPENSSL_BIN} base64 -a -A`
-				
-	# assemble a request to convert XML to JSON and send it over the Admin API
-	REQ=`create_convert_json "IDP" "SAML20" "${B64XML}"`
-	RESPONSE=`api_request connectionMetadata/convert POST "${REQ}"` || exit
+# get the connection with the specified entity id from the (global) list of (JSON) connections retrieved from PF earlier
+function conn_get_by_entityid() {
+	local ENTITYID=$1
+	echo "${GLOB_ALL_CONS}" | ${JQ_BIN} --arg entity ${ENTITYID} '.items[] | select(.entityId==$entity)'
+}
 
+# convert SAML 2.0 metadata XML to PF JSON connection description using the PF connectionMetadata/convert endpoint
+function conn_xml_to_json {
+	local XML_FILE=$1
+	local TYPE=$2
+	local TEMPLATE_CONN_JSON=$3
+
+	# base64encode the entity descriptor XML
+	B64XML=`cat ${XML_FILE} | ${OPENSSL_BIN} base64 -a -A`
+
+	# assemble a request to convert XML to JSON and send it over the Admin API	
+	REQ=`cat <<EOF
+{
+ "connectionType" : "${TYPE}",
+ "expectedProtocol" : "SAML20",
+ "samlMetadata" : "${B64XML}",
+ "templateConnection" : ${TEMPLATE_CONN_JSON}
+}
+EOF`
+	RESPONSE=`api_request connectionMetadata/convert POST "${REQ}"` || exit 1
+
+	echo "${RESPONSE}"
+}
+
+# create a connection from an EntityDescriptor in an XML file using the Admin REST API
+function conn_idp_create() {
+	local FILE=$1
+	local TEMPLATE_CONN_JSON=$2
+
+	# convert the XML to a JSON connection description using the PF API
+	RESPONSE=`conn_xml_to_json "${FILE}" "IDP" "${TEMPLATE_CONN_JSON}"` || exit 1
+
+	# extract the attribute contract from the template connection (TODO: workaround because it gets lost??)
+	CONTRACT=`echo ${TEMPLATE_CONN_JSON} | ${JQ_BIN} '.idpBrowserSso.attributeContract'`
+	
 	# assemble a connection create request 
 	REQ=`echo "{ \"conn\": ${RESPONSE}, \"contract\": ${CONTRACT} }" | ${JQ_BIN} '.conn.connection.idpBrowserSso.attributeContract=.contract | .conn.connection | .name="[P] "+ .entityId | del(.credentials.signingSettings)'`
-					
+
 	# send the request over the Admin API
 	# NB: do not exit if creation/validation fails
-	RESPONSE=`api_request sp/idpConnections POST "${REQ}"`
-
+	RESPONSE=`api_request sp/idpConnections POST "${REQ}" > /dev/null` || return
+	
 	printf "OK\n"
 }
 	
-# update certificates for a connection from an EntityDescriptor in an XML file using the Admin REST API
-conn_idp_update() {
+# update a connection from an EntityDescriptor in an XML file using the Admin REST API
+function conn_idp_update() {
 	local FILE=$1
 	local ENTITYID=$2
 
-	# TODO: REFACTOR NEXT 2 PARAGRAPHS IN TO FUNCTION SHARED WITH conn_idp_delete
-
 	# find the connection by its entityId in the list of all connections
-	CONN=`echo "${ALL_CONNS}" | ${JQ_BIN} --arg entity ${ENTITYID} '.items[] | select(.entityId==$entity)'`
-					
+	CONN=`conn_get_by_entityid "${ENTITYID}"`
 	# see if we have a match
 	if [ -z "${CONN}" ] ; then
-		echo " [SKIP]\n"
+		printf " [SKIP]\n"
 		return
 	fi
 
-	TEMPLATE_CONN=`echo "${CONN}" | ${JQ_BIN} 'del(.credentials.certs)'`
-	CONTRACT=`echo ${TEMPLATE_CONN} | ${JQ_BIN} '.idpBrowserSso.attributeContract'`
+	# override the global template variables, since the template is now the existing connection
+	TEMPLATE_CONN_JSON=`echo "${CONN}" | ${JQ_BIN} 'del(.credentials.certs)'`
+	# extract the attribute contract from the template connection (TODO: workaround because it gets lost??)
+	CONTRACT=`echo "${TEMPLATE_CONN_JSON}" | ${JQ_BIN} '.idpBrowserSso.attributeContract'`
 
-	# TODO: REFACTOR NEXT 2 PARAGRAPHS IN TO FUNCTION SHARED WITH conn_idp_create
+	# convert the XML to a JSON connection description using the PF API
+	RESPONSE=`conn_xml_to_json "${FILE}" "IDP" "${TEMPLATE_CONN_JSON}"` || exit 1
 
-	# base64encode the entity descriptor XML
-	B64XML=`cat ${FILE} | ${OPENSSL_BIN} base64 -a -A`
-				
-	# assemble a request to convert XML to JSON and send it over the Admin API
-	REQ=`create_convert_json "IDP" "SAML20" "${B64XML}"`									
-	RESPONSE=`api_request connectionMetadata/convert POST "${REQ}"` || exit
-
-	CERTS=`echo "${RESPONSE}" | ${JQ_BIN} '.connection.credentials.certs | if (. | length) > 1 then .[0].secondaryVerificationCert = true | .[1].primaryVerificationCert = true else .[0].secondaryVerificationCert = false end'`
-	#REQ=`echo "{ \"conn\": ${CONN}, \"certs\": ${CERTS} }"  | ${JQ_BIN} '.conn.credentials.certs=.certs | .conn'`	
+	# some hacking on the verification cert labels
+	CERTS=`echo "${RESPONSE}" | ${JQ_BIN} '.connection.credentials.certs | if (. | length) > 1 then .[0].secondaryVerificationCert = true | .[1].primaryVerificationCert = true else . end'`
+	# set/override the certs & contract in the PUT request
 	REQ=`echo "{ \"conn\": ${RESPONSE}, \"contract\": ${CONTRACT}, \"certs\": ${CERTS} }" | ${JQ_BIN} '.conn.connection.idpBrowserSso.attributeContract=.contract | .conn.connection.credentials.certs=.certs | .conn.connection'`
+	#REQ=`echo "{ \"conn\": ${RESPONSE}, \"contract\": ${CONTRACT} }" | ${JQ_BIN} '.conn.connection.idpBrowserSso.attributeContract=.contract | .conn.connection'`
 
 	# assemble and send a connection update request over the Admin API
 	ID=`echo "${CONN}" | ${JQ_BIN} -r '.id'`
-	#update certs
-	RESPONSE=`api_request sp/idpConnections/${ID} PUT "${REQ}"` || exit
 	
+	RESPONSE=`api_request "sp/idpConnections/${ID}" PUT "${REQ}" > /dev/null` || return
+
 	printf "OK\n"
 }
 
-conn_idp_delete() {
+function conn_idp_delete() {
 	local ENTITYID=$1
 	
 	# find the connection by its entityId in the list of all connections
-	CONN=`echo "${ALL_CONNS}" | ${JQ_BIN} --arg entity ${ENTITYID} '.items[] | select(.entityId==$entity)'`
-					
+	CONN=`conn_get_by_entityid "${ENTITYID}"`
 	# see if we have a match
 	if [ -z "${CONN}" ] ; then
-		echo " [SKIP]\n"
+		printf " [SKIP]\n"
 		return
 	fi
 		
 	# assemble and send a connection deactivation request over the Admin API
 	ID=`echo "${CONN}" | ${JQ_BIN} -r '.id'`
-	CONN=`echo "${CONN}" | ${JQ_BIN} '.active=false'`						
-	RESPONSE=`api_request sp/idpConnections/${ID} PUT "${CONN}"` || exit
-						
+	CONN=`echo "${CONN}" | ${JQ_BIN} '.active=false'`
+	RESPONSE=`api_request sp/idpConnections/${ID} PUT "${CONN}"`
+
 	# next send a connection delete request over the Admin API
 	# NB: do not exit on suspicious responses
 	RESPONSE=`api_request sp/idpConnections/${ID} "DELETE"`
@@ -275,29 +288,27 @@ conn_idp_delete() {
 	# should return HTTP 204 with an empty response, check that
 	if [ -n "${RESPONSE}" ] ; then
 		printf "ERROR\n"
-		echo ${RESPONSE}
-		exit
+		printf "${RESPONSE}\n"
+		return
 	fi
 
 	printf " OK\n"
 }
 
 # count the number of EntityDescriptors
-COUNT=`exec_xmllint "${TMP1}" "dir /md:EntitiesDescriptor/md:EntityDescriptor" "ELEMENT EntityDescriptor" | wc -l`
+COUNT=`exec_xmllint "${TMP_ENTITIES}" "dir /md:EntitiesDescriptor/md:EntityDescriptor" "ELEMENT EntityDescriptor" | wc -l`
 
 # get the list of all current connections from PingFederate
-ALL_CONNS=`api_request sp/idpConnections` || (echo "ERROR: could not obtain list of connections from PingFederate" && exit)
+GLOB_ALL_CONS=`api_request sp/idpConnections` || (echo "ERROR: could not obtain list of connections from PingFederate" && exit 1)
 
 # see if we need to obtain info about a template connection
 if [[ $ACTION == "create" ]] ; then
 	# find the template connection
-	TEMPLATE_CONN=`echo "${ALL_CONNS}" | ${JQ_BIN} --arg entity ${TEMPLATE} '.items[] | select(.entityId==$entity)'`
-	if [ -z "${TEMPLATE_CONN}" ] ; then
-		echo "ERROR: template connection \"${TEMPLATE}\" not found"
-		exit
+	TEMPLATE_CONN_JSON=`conn_get_by_entityid "${TEMPLATE_ENTITY_ID}"`
+	if [ -z "${TEMPLATE_CONN_JSON}" ] ; then
+		echo "ERROR: template connection \"${TEMPLATE_ENTITY_ID}\" not found"
+		exit 1
 	fi
-	# extract the attribute contract from the template connection (workaround because it gets lost??)
-	CONTRACT=`echo ${TEMPLATE_CONN} | ${JQ_BIN} '.idpBrowserSso.attributeContract'`
 fi
 
 # loop over the EntityDescriptors inside the EntitiesDescriptor
@@ -305,31 +316,31 @@ let i=1
 while [ $i -le ${COUNT} ]; do
 
 	# store the single entity descriptor in a temporary XML file
-	exec_xmllint ${TMP1} "cat /md:EntitiesDescriptor/md:EntityDescriptor[$i]" | ${SED_BIN} "1,1d; $ d" > ${TMP2}
-
-	# grab the entityId	
-	ENTITY=`exec_xmllint ${TMP2} "cat /md:EntityDescriptor/@entityID" "entityID" | cut -d"\"" -f2`
+	exec_xmllint ${TMP_ENTITIES} "cat /md:EntitiesDescriptor/md:EntityDescriptor[$i]" | ${SED_BIN} '1d;$d' > ${TMP_ENTITY} || exit 1
 	
+	# grab the entityId	
+	ENTITY_ID=`exec_xmllint ${TMP_ENTITY} "cat /md:EntityDescriptor/@entityID" "entityID" | cut -d"\"" -f2` || exit 1
+
 	# print out which entity we are processing
-	printf "[$i] : processing [${ENTITY}] ..."
+	printf "[$i] : processing [${ENTITY_ID}] ..."
 
 	# check if it is an IDP	
-	if exec_xmllint ${TMP2} "dir /md:EntityDescriptor/md:IDPSSODescriptor" "ELEMENT IDPSSODescriptor" >/dev/null ; then 
+	if exec_xmllint ${TMP_ENTITY} "dir /md:EntityDescriptor/md:IDPSSODescriptor" "ELEMENT IDPSSODescriptor" >/dev/null ; then 
 
 		printf " [IDP] "
 
 		# check if it is a SAML 2.0 descriptor
-		if exec_xmllint ${TMP2} "cat /md:EntityDescriptor/md:IDPSSODescriptor/@protocolSupportEnumeration" "protocolSupportEnumeration" | ${GREP_BIN} "urn:oasis:names:tc:SAML:2.0:protocol" > /dev/null ; then
+		if exec_xmllint ${TMP_ENTITY} "cat /md:EntityDescriptor/md:IDPSSODescriptor/@protocolSupportEnumeration" "protocolSupportEnumeration" | ${GREP_BIN} "urn:oasis:names:tc:SAML:2.0:protocol" > /dev/null ; then
 
 			case "${ACTION}" in
 				"create")
-					conn_idp_create "${TMP2}"
+					conn_idp_create "${TMP_ENTITY}" "${TEMPLATE_CONN_JSON}"
 					;;
 				"update")
-					conn_idp_update "${TMP2}" "${ENTITY}"
+					conn_idp_update "${TMP_ENTITY}" "${ENTITY_ID}"
 					;;
 				"delete")
-					conn_idp_delete "${ENTITY}"
+					conn_idp_delete "${ENTITY_ID}"
 					;;
 				*)
 					echo "ERROR: unsupported action: \"${ACTION}\""
