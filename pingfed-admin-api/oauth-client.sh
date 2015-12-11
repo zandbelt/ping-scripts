407a6d77c325a50b0d8e6040d5a121f63819dfa1
Hans Zandbelt <hzandbelt@pingidentity.com>
2015-12-11 10:08:50 +0100

    add sample script to manage OAuth clients (with client cert auth)

---
+++ pingfed-admin-api/oauth-client.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# sample script to manage OAuth clients
+
+ADM_USER=administrator
+ADM_PWD=2Federate
+PF_API=https://localhost:9999/pf-admin-api/v1
+
+FLAGS="-k -s -u \"${ADM_USER}:${ADM_PWD}\" -H \"X-XSRF-Header: pingfed\""
+
+case $1 in
+	list)
+		echo ${FLAGS} | xargs curl ${PF_API}/oauth/clients
+		;;
+	get)
+		echo ${FLAGS} | xargs curl ${PF_API}/oauth/clients/${2}
+		;;
+	delete)
+		echo ${FLAGS} | xargs curl -X DELETE ${PF_API}/oauth/clients/${2}
+		;;
+	create)
+		JSON_DATA=`cat <<JSON
+{
+  "clientId": "${2}",
+  "redirectUris": [
+    "https://localhost:9031/OAuthPlayground/case1A-callback.jsp"
+  ],
+  "grantTypes": [
+    "AUTHORIZATION_CODE",
+    "REFRESH_TOKEN"
+  ],
+  "name": "${2}",
+   "clientAuth": {
+    "type": "CERTIFICATE",
+    "clientCertIssuerDn": "CN=localhost, OU=Development, O=PingIdentity, L=Denver, ST=CO, C=US",
+    "clientCertSubjectDn": "CN=${2}"
+  }
+}JSON`
+		echo ${FLAGS} | xargs curl -H "Content-Type: application/json" --data-binary "${JSON_DATA}" ${PF_API}/oauth/clients
+		;;
+	*)
+		echo "Usage: $0 [ list | get <id> | delete <id> | create <id>"
+		;;		
+esac
