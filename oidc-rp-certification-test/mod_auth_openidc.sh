6224b6c90e043288a4f6df6d16b90e67175b8be0
Hans Zandbelt <hzandbelt@pingidentity.com>
2016-09-14 09:43:51 +0200

    updated tests

--- oidc-rp-certification-test/mod_auth_openidc.sh
+++ oidc-rp-certification-test/mod_auth_openidc.sh
@@ -10,11 +10,15 @@
 #
 ###########################################################################
 
-# https://rp.certification.openid.net:8080/mod_auth_openidc/rp-response_type-code
-
 REDIRECT_URI="<THE-REDIRECT_URI-OF-YOUR-APACHE-MOD_AUTH_OPENIDC-INSTANCE>"
 TARGET_URL="<YOUR-APPLICATION-URL-PROTECTED-BY-MOD_AUTH_OPENIDC>"
 RP_ID="<YOUR-RP-TEST-CLIENT-IDENTIFIER>"
+
+SETENV="$(dirname "$0")/setenv.sh"
+if [[ -x "${SETENV}" ]]; then
+	source "${SETENV}"
+fi
+
 RP_TEST_URL="https://rp.certification.openid.net:8080"
 COOKIE_JAR="/tmp/cookie.jar"
 FLAGS="-s -k -b ${COOKIE_JAR} -c ${COOKIE_JAR}"
@@ -25,6 +29,7 @@ TESTS="
 	rp_discovery_issuer_not_matching_config
 	rp_discovery_openid_configuration
 	rp_discovery_jwks_uri_keys
+	rp_discovery_webfinger_unknown_member
 "
 
 if [ -z $1 ] ; then
@@ -50,7 +55,7 @@ function exec_discovery() {
 }
 
 function rp_discovery_webfinger_url() {
-	local TEST_ID="rp-discovery-webfinger_url"
+	local TEST_ID="rp-discovery-webfinger-url"
 	local CSRF=$1
 	local ISSUER="${RP_TEST_URL}/${RP_ID}/${TEST_ID}"
 
@@ -59,12 +64,12 @@ function rp_discovery_webfinger_url() {
 	
 	echo " * "
 	echo " * [server] check that the registration is initiated to the discovered endpoint: ${RP_ID}/${TEST_ID}/registration"
-	echo " * [server] the registration itself will fail with an \"incorrect_behavior\" response"
+	echo " * [server] the registration itself will fail with an \"incorrect_behavior: You should not talk to this endpoint in this test\""
 	echo " * "
 }
 
 function rp_discovery_webfinger_acct() {
-	local TEST_ID="rp-discovery-webfinger_acct"
+	local TEST_ID="rp-discovery-webfinger-acct"
 	local CSRF=$1
 	local DOMAIN=`echo ${RP_TEST_URL} | cut -d"/" -f3`
 	local ACCT="${RP_ID}.${TEST_ID}@${DOMAIN}"
@@ -79,7 +84,7 @@ function rp_discovery_webfinger_acct() {
 }
 
 function rp_discovery_issuer_not_matching_config() {
-	local TEST_ID="rp-discovery-issuer_not_matching_config"
+	local TEST_ID="rp-discovery-issuer-not-matching-config"
 	local CSRF=$1
 	local ISSUER="${RP_TEST_URL}/${RP_ID}/${TEST_ID}"
 
@@ -92,7 +97,7 @@ function rp_discovery_issuer_not_matching_config() {
 }
 
 function rp_discovery_openid_configuration() {
-	local TEST_ID="rp-discovery-openid_configuration"
+	local TEST_ID="rp-discovery-openid-configuration"
 	local CSRF=$1
 	local ISSUER="${RP_TEST_URL}/${RP_ID}/${TEST_ID}"
 
@@ -105,17 +110,42 @@ function rp_discovery_openid_configuration() {
 }
 
 function rp_discovery_jwks_uri_keys() {
-	local TEST_ID="rp-discovery-jwks_uri_keys"
+	local TEST_ID="rp-discovery-jwks_uri-keys"
 	local CSRF=$1
 	local ISSUER="${RP_TEST_URL}/${RP_ID}/${TEST_ID}"
 
 	echo " [${TEST_ID}] initiate Discovery..."
-	exec_discovery ${TEST_ID} ${CSRF} ${ISSUER} | grep "Location:"
+	REQUEST=`exec_discovery ${TEST_ID} ${CSRF} ${ISSUER} | grep -i "Location:" | cut -d" " -f2 | tr -d '\r' | cut -d"#" -f2`
+	echo " [${TEST_ID}] send authentication request to OP..."
+	RESPONSE=`echo ${FLAGS} -i | xargs curl "${REQUEST}" | grep -i "Location:" | cut -d" " -f2 | tr -d '\r' | cut -d"#" -f2`
+	echo " [${TEST_ID}] return authentication response to RP..."
+	RETURN=`echo ${FLAGS} -i | xargs curl "${RESPONSE}" | grep -i "Location:" | cut -d" " -f2 | tr -d '\r' | cut -d"#" -f2`
+	printf " [${TEST_ID}] access application as authenticated user..."
+	echo ${FLAGS} | xargs curl "${RETURN}" | grep -q "\[OIDC_CLAIM_sub\]"
+	if [ $? -eq 0 ]; then echo "SUCCESS"; else echo "ERROR"; fi
 
 	echo " * "
-	echo " * [client] TODO: don't how to proceed here: can't register a client since that produces \"incorrect_behavior\""
+	echo " * [server] check that the id_token returned by the OP verifies correctly with the discovered key"
+	echo " * [client] check that access the to application is granted as an authenticated user (\"SUCCESS\")"
 	echo " * "
 }
+
+ 
+function rp_discovery_webfinger_unknown_member() {
+	local TEST_ID="rp-discovery-webfinger-unknown-member"
+	local CSRF=$1
+	local DOMAIN=`echo ${RP_TEST_URL} | cut -d"/" -f3`
+	local ACCT="${RP_ID}.${TEST_ID}@${DOMAIN}"
+
+	echo " [${TEST_ID}] initiate Discovery..."
+	exec_discovery ${TEST_ID} ${CSRF} ${ACCT} | grep "Location:"
+
+	echo " * "
+	echo " * [server] check that the webfinger request contains acct: and the response contains \"dummy\": \"foobar\""
+	echo " * [client] check that the authentication request is initiated to the discovered authorization endpoint with the login_hint set to the acct: value"
+	echo " * "
+}
+
 
 exec_init $1
 
