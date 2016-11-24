3ee5b6d9c1be58516ff7cf383f0bd2f802caf20b
Hans Zandbelt <hzandbelt@pingidentity.com>
2016-11-24 18:04:28 +0100

    allow passing in PFBASE as an environment variable

--- hypersonic.sh
+++ hypersonic.sh
@@ -4,7 +4,10 @@
 # connect the internal Swing DB GUI with the default PF database
 #
 
-PFBASE="/Users/hzandbelt/pingfederate/deploy/oauthplayground-3.3.0/pingfederate-8.2.0"
+if [ -z $PFBASE ] ; then
+  PFBASE="/Users/hzandbelt/pingfederate/deploy/oauthplayground-3.3.0/pingfederate-8.2.2"
+fi
+
 URL="jdbc:hsqldb:file:${PFBASE}/pingfederate/server/default/data/hypersonic/PFDefaultDB"
 USER="sa"
 PWD="secretpass"
