3b5fef6c8edff312a8a44a8826efe1c87d60797f
Hans Zandbelt <hzandbelt@pingidentity.com>
2016-11-30 18:19:49 +0000

    doc the jwt-at-as-id_token flow

--- ropc-oidc.sh
+++ ropc-oidc.sh
@@ -3,10 +3,122 @@
 ###########################################################################
 # Copyright (C) 2016 Ping Identity Corporation
 #
+# @Author: Hans Zandbelt - hzandbelt@pingidentity.com
+#
 # Script to demonstrate the alternatives for leveraging the Resource Owner
 # Password Credentials (ROPC) grant type in an OpenID Connect fashion.
+# Alternatives are:
+#
+# 1. "userinfo"
+# Access the OIDC user info endpoint with the access token obtained in a regular
+# OAuth 2.0 flow by an OIDC client (where the openid scope will not return an id_token)
+# - standard ROPC and obtain claims as regular JSON
+#
+# 2. "implicit"
+# Mimic a browser and post the username/password credentials against the authorization endpoint
+# and obtain the id_token from the Location header that is returned
+# - non-standard ROPC and obtain claims in id_token
 # 
-# @Author: Hans Zandbelt - hzandbelt@pingidentity.com
+# 3. "jwt"
+# Obtain a JWT access token that is decorated as an id_token (8.2 allows for a jwks_uri to validate)
+# - standard ROPC and obtain claims in a signed JWT that cannot be distinguished from an id_token
+#
+#
+# Sample Output:
+#
+# $ ./ropc-oidc.sh implicit | jq -S '.'
+# OIDC Implicit grant...
+# {
+#   "at_hash": "V5Z5xEw7atBZqbtAznEVGQ",
+#   "aud": "im_oic_client",
+#   "auth_time": 1480529154,
+#   "exp": 1480529454,
+#   "iat": 1480529154,
+#   "iss": "https://localhost:9031",
+#   "jti": "mBUxAzaeBgoc482D0Gs119",
+#   "nonce": "nonce",
+#   "sub": "joe"
+# }
+#
+# $ ./ropc-oidc.sh jwt | jq -S '.'
+# ROPC for JWT access token...
+# Got JWT access token "eyJhbGciOiJSUzI1NiIsImtpZCI6Imp3dCJ9.eyJzdWIiOiJqb2UiLCJpYXQiOjE0ODA1MjkxNTgsIm5vbmNlIjoiZnVqZlh6S09YeiIsImp0aSI6InJ0Qm5HaDhlMlExVUljUUNoanNXbEEiLCJhdXRoX3RpbWUiOjE0ODA1MjkxNTgsImV4cCI6MTQ4MDUzNjM1OCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6OTAzMSIsImF1ZCI6ImltX29pY19jbGllbnQifQ.BNQ9QnvT9eUACOr0e_WhcST4KlwjNSqfLh8iHPWmlG0ZXAW0ipZolFw8fu8HmCO7MF6y4I580Ia0TbTygpct6hDB4UAiGOZZ9nmGnTj3YMAdlRH6Jv-c9jeN5_ab-FSmxS9MVrlFo0C21YJ54deHG5vWZK7DQEfdruR2gIdylLVUE49Fm2sQuP6tPLV6_cilGIXakBYMf0GVz49HQsnPS3PBaxH3jHS7aVGnImZYa_K9Rm-tru6lJ1wnk1lQ-qI7Tpq4qt43MayBjhF5Ig_iY7TvpVNpsaorc_H6E0DYs9OdkvMFVO0xKK_qf2VG9RyqMMia3iM2KUQaJvRdmb1Xzw"...
+# Decoded JWT access token payload...
+# {
+#   "aud": "im_oic_client",
+#   "auth_time": 1480529158,
+#   "exp": 1480536358,
+#   "iat": 1480529158,
+#   "iss": "https://localhost:9031",
+#   "jti": "rtBnGh8e2Q1UIcQChjsWlA",
+#   "nonce": "fujfXzKOXz",
+#   "sub": "joe"
+# }
+#
+#
+# With "jwt" settings in access token manager set to:
+#
+# Access Token Management Configuration Summary
+# Create Access Token Management Instance
+# Type	
+# Instance Name	jwt
+# Instance Id	jwt
+# Type	JSON Web Tokens
+# Class Name	com.pingidentity.pf.access.token.management.plugins.JwtBearerAccessTokenManagementPlugin
+# Parent Instance Name	None
+# Instance Configuration	
+# Certificates	jwt, CN=jwt, O=jwt, C=US
+# Token Lifetime	120
+# JWS Algorithm	RSA using SHA-256
+# Active Symmetric Key ID	None Selected
+# Active Signing Certificate Key ID	jwt
+# JWE Algorithm	None Selected
+# JWE Content Encryption Algorithm	None Selected
+# Active Symmetric Encryption Key ID	None Selected
+# Asymmetric Encryption Key	
+# Asymmetric Encryption JWKS URL	
+# Include Key ID Header Parameter	true
+# Include X.509 Thumbprint Header Parameter	false
+# Default JWKS URL Cache Duration	720
+# Include JWE Key ID Header Parameter	true
+# Include JWE X.509 Thumbprint Header Parameter	false
+# Client ID Claim Name	
+# Scope Claim Name	
+# Space Delimit Scope Values	false
+# Issuer Claim Value	https://localhost:9031
+# Audience Claim Value	im_oic_client
+# JWT ID Claim Length	0
+# Access Grant GUID Claim Name	
+# JWKS Endpoint Path	
+# JWKS Endpoint Cache Duration	720
+# Publish Key ID X.509 URL	false
+# Publish Thumbprint X.509 URL	false
+# Access Token Attribute Contract	
+# Attribute	auth_time
+# Attribute	iat
+# Attribute	jti
+# Attribute	nonce
+# Attribute	sub
+# Resource URIs	
+# Access Control	
+# Restrict Allowed Clients	false
+#
+#
+# And "jwt" access token mapping settings to:
+#
+# Mapping Summary
+# Access Token Mapping
+# Attribute Sources & User Lookup	
+# Data Sources	(None)
+# Contract Fulfillment	
+# sub	username (Password Credential Validator)
+# auth_time	@java.lang.Long@valueOf(@org.jose4j.jwt.NumericDate@now().getValue())
+# (Expression)
+# iat	@java.lang.Long@valueOf(@org.jose4j.jwt.NumericDate@now().getValue()) (Expression)
+# nonce	@org.sourceid.common.IDGenerator@rndAlphaNumeric(10) (Expression)
+# jti	@org.sourceid.common.IDGenerator@rndAlphaNumeric(22) (Expression)
+# Issuance Criteria	
+# Criterion	(None)
 #
 ###########################################################################
 
@@ -66,8 +178,8 @@ case $1 in
 
     #
     # prerequisites: OAuth 3.x Playground installed
-    #                "jwt" JWT Token Manager instance has been created
-    #                Access Token Mapping has been created for "jwt" and UserPass PCV
+    #                "jwt" JWT Token Manager instance has been created (see top of script)
+    #                Access Token Mapping has been created for "jwt" and UserPass PCV (see top of script)
     #
 
     TOKEN_MGR="jwt"
