73f132cbd355301c08927100c207530cec860272
Hans Zandbelt <hzandbelt@pingidentity.com>
2017-01-20 08:33:33 +0000

    add authorization code sample and update client secret sample

--- pingfed-admin-api/oauth-client.sh
+++ pingfed-admin-api/oauth-client.sh
@@ -9,16 +9,20 @@ PF_API=https://localhost:9999/pf-admin-api/v1
 FLAGS="-k -s -u \"${ADM_USER}:${ADM_PWD}\" -H \"X-XSRF-Header: pingfed\""
 
 case $1 in
+
 	list)
 		echo ${FLAGS} | xargs curl ${PF_API}/oauth/clients
 		;;
+
 	get)
 		echo ${FLAGS} | xargs curl ${PF_API}/oauth/clients/${2}
 		;;
+
 	delete)
 		echo ${FLAGS} | xargs curl -X DELETE ${PF_API}/oauth/clients/${2}
 		;;
-	create)
+
+	create-cc)
 		#"redirectUris": [ "https://localhost:9031/OAuthPlayground/case1A-callback.jsp" ],	
 		JSON_DATA=`cat <<JSON
 {
@@ -40,7 +44,50 @@ case $1 in
 JSON`
 		echo ${FLAGS} | xargs curl -H "Content-Type: application/json" --data-binary "${JSON_DATA}" ${PF_API}/oauth/clients
 		;;
+
+	create-ac)
+		JSON_DATA=`cat <<JSON
+   {
+      "clientId": "${2}",
+      "redirectUris": [
+        "https://localhost:9031/OAuthPlayground/case1-callback.jsp"
+      ],
+      "grantTypes": [
+        "AUTHORIZATION_CODE",
+        "REFRESH_TOKEN"
+      ],
+      "name": "Authorization Code Client ${2}",
+      "description": "",
+      "logoUrl": "",
+      "refreshRolling": "SERVER_DEFAULT",
+      "persistentGrantExpirationType": "SERVER_DEFAULT",
+      "persistentGrantExpirationTime": 0,
+      "persistentGrantExpirationTimeUnit": "DAYS",
+      "bypassApprovalPage": false,
+      "restrictScopes": false,
+      "restrictedScopes": [],
+      "validateUsingAllEligibleAtms": false,
+      "oidcPolicy": {
+        "grantAccessSessionRevocationApi": false,
+        "pingAccessLogoutCapable": false
+      },
+      "clientAuth": {
+        "type": "SECRET",
+        "secret": "2Federate"
+      }
+    }
+JSON`
+		echo ${FLAGS} | xargs curl -H "Content-Type: application/json" --data-binary "${JSON_DATA}" ${PF_API}/oauth/clients        
+		;;
+
+	update-client-secret)	
+		JSON_DATA=`echo ${FLAGS} | xargs curl ${PF_API}/oauth/clients/${2} | jq \
+		". | del(.clientAuth.encryptedSecret) | .clientAuth.secret = \"${3}\" \
+		"`
+		echo ${FLAGS} | xargs curl -v -H "Content-Type: application/json" -X PUT --data-binary "${JSON_DATA}" ${PF_API}/oauth/clients/${2}		
+		;;
+
 	*)
-		echo "Usage: $0 [ list | get <id> | delete <id> | create <id>"
+		echo "Usage: $0 [ list | get <id> | delete <id> | create-cc <id> | create-ac <id> | update-client-secret <id> <secret>"
 		;;		
 esac
