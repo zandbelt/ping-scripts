3fd399967725681ac0184a5adaeb4b12d2595898
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2017-09-28 11:41:24 +0200

    support for SPs as well as IDPs
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- pingfed-admin-api/check-certs-expiry.sh
+++ pingfed-admin-api/check-certs-expiry.sh
@@ -8,27 +8,33 @@ PF_API="https://localhost:9999/pf-admin-api/v1"
 
 FLAGS="-k -S -s -u \"${ADM_USER}:${ADM_PWD}\" -H \"X-XSRF-Header: pingfed\""
 
+pf_check_expiry() {
+  local ENDPOINT="$1"
+  local DAYS="$2"
+  if [ -z "${DAYS}" ] ; then DAYS=30; fi
+  echo ${FLAGS} | xargs curl ${PF_API}/${ENDPOINT} | jq '.items[] | .entityId as $entityid | .credentials.certs[] | [$entityid, .x509File.fileData] | join("|")' |
+    while IFS="|" read -r entityid cert ; do
+      entityid=$(echo "$entityid" | tr -d '"')
+      cert=$(echo "$cert" | tr -d '"')
+      #echo "entityid = *${entityid}*"
+      #echo "cert = *${cert}*"
+      enddate=$(echo -e "$cert" | openssl x509 -checkend $(( 86400 * DAYS )) -enddate)
+      if [[ $enddate =~ (.*)Certificate\ will\ expire ]]; then
+        echo "Verification certificate for \"$entityid\" has expired or will do so within the next $DAYS day(s)!"
+      else
+        echo "Verification certificate for \"$entityid\" is good for another $DAYS day(s)."
+      fi
+    done
+}
+
 case $1 in
 	idps)
-		DAYS=30
-		if [ -n "$2" ] ; then
-			DAYS=$2
-		fi
-		echo ${FLAGS} | xargs curl ${PF_API}/sp/idpConnections | jq '.items[] | .entityId as $entityid | .credentials.certs[] | [$entityid, .x509File.fileData] | join("|")' |
-		  while IFS="|" read -r entityid cert ; do
-		    entityid=$(echo "$entityid" | tr -d '"')
-		    cert=$(echo "$cert" | tr -d '"')
-		    #echo "entityid = *${entityid}*"
-		    #echo "cert = *${cert}*"
-		    enddate=$(echo -e "$cert" | openssl x509 -checkend $(( 86400 * DAYS )) -enddate)
-		    if [[ $enddate =~ (.*)Certificate\ will\ expire ]]; then
-		      echo "Verification certificate for \"$entityid\" has expired or will do so within the next $DAYS day(s)!"
-		    else
-		      echo "Verification certificate for \"$entityid\" is good for another $DAYS day(s)."
-		    fi
-		  done
+	    pf_check_expiry "sp/idpConnections" "$2"
+		;;
+	sps)
+	    pf_check_expiry "idp/spConnections" "$2"
 		;;
 	*)
-		echo "Usage: $0 [ idps [<days>] ] "
+		echo "Usage: $0 [ idps [<days>] | sps [<days>] ] "
 		;;
 esac
